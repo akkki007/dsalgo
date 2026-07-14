cbb48b94088649f872c62c991d8ffb2136b01d1f
Akshay Nazare <115125778+akkki007@users.noreply.github.com>
2026-07-14 11:06:51 +0530

    Time: 0 ms (100%), Space: 11.5 MB (82.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxLen = 0,n=s.length();
+        int l=0,maxlen=0,n=s.length();
         int hash[256];
-        for(int i=0;i<256;i++){
-            hash[i] = -1;
-        }
-        int l = 0, r = 0;
-        while(r<n){
-            if(hash[s[r]]!=-1){ // has this character appeared before,anywhere in the string ?
-                if(hash[s[r]]>=l){ // okay it appears but is that old occurence still inside my current window ? if yes then update the left pointer
-                    l = hash[s[r]]+1;
-                }
+        memset(hash,-1,sizeof(hash));
+
+        for(int r=0;r<n;r++){
+            unsigned char c = s[r];
+            if(hash[c]>=l){
+                l = hash[c] + 1;
             }
-            int len = r - l + 1;
-            maxLen = max(len,maxLen);
-            hash[s[r]] = r;
-            r++;
+            maxlen = max(maxlen,r-l+1);
+            hash[c] = r;
         }
-    return maxLen;
+        return maxlen;
     }
 };
