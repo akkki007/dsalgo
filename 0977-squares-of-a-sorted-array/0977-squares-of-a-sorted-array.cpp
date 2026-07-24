788c66145394569e2fb3d38fb0863426b09e4a68
Akshay Nazare <115125778+akkki007@users.noreply.github.com>
2026-07-24 21:36:26 +0530

    Time: 0 ms (100%), Space: 30.2 MB (87.62%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -5,8 +5,24 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             res[i] = nums[i] * nums[i];
         }
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int pos = n-1;
+        
+        while(left<=right){
+            if(abs(nums[left])>abs(nums[right])){
+                res[pos] = nums[left]*nums[left];
+                left++;
+            }else{
+                res[pos] = nums[right]*nums[right];
+                right--;
+            }
+        pos--;
+        }
 
-        sort(res.begin(), res.end());
+
+        
         return res;
     }
 };
