5dcb56e4e4de68d87828331f6c548223630802ae
Akshay Nazare <115125778+akkki007@users.noreply.github.com>
2026-07-28 07:25:49 +0530

    Time: 0 ms (100%), Space: 20.1 MB (11.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> freq;
+        for(int num : nums) freq[num]++;
 
-        for(int num:nums){
-            freq[num]++;
+        // bucket[i] holds all numbers that appear exactly i times
+        // max possible frequency is n, so we need n+1 buckets (index 0..n)
+        int n = nums.size();
+        vector<vector<int>> bucket(n + 1);
+        for(auto& [num, count] : freq) {
+            bucket[count].push_back(num);
         }
 
-        vector<pair<int, int>> v(freq.begin(),freq.end());
-
-        sort(v.begin(),v.end(),[](auto &a, auto &b){
-            return a.second > b.second;
-        });
-        
+        // walk from highest frequency down, collecting until we have k
         vector<int> ans;
-        for(int i=0;i<k;i++){
-            ans.push_back(v[i].first);
+        for(int i = n; i >= 1 && ans.size() < k; i--) {
+            for(int num : bucket[i]) {
+                ans.push_back(num);
+                if(ans.size() == k) break;
+            }
         }
-
         return ans;
     }
 };
