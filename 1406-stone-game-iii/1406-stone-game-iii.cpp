ce824abbec1a9400757194c977a8d5d95a0fb4a7
Akshay Nazare <115125778+akkki007@users.noreply.github.com>
2026-08-04 05:50:27 +0530

    Time: 36 ms (48.01%), Space: 141.5 MB (47.55%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
+    // Initializing DP vector
     vector<int> dp;
 
-    int solve(int i, int n, vector<int>& nums) {
-
-        if (i >= n)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
+    int solve(int i,int n,vector<int>& nums){
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
 
         int ans = INT_MIN;
 
-        // Take 1 stone
-        int take1 = nums[i] - solve(i + 1, n, nums);
-        ans = max(ans, take1);
+        // take 1
+        int take1 = nums[i] - solve(i+1,n,nums);
+        ans = max(ans,take1);
 
-        // Take 2 stones
-        if (i + 1 < n) {
-            int take2 = nums[i] + nums[i + 1] - solve(i + 2, n, nums);
-            ans = max(ans, take2);
+        // take 2
+        if(i+1 < n){
+            int take2 = nums[i] + nums[i+1] - solve(i+2,n,nums);
+            ans = max(ans,take2);
         }
 
-        // Take 3 stones
-        if (i + 2 < n) {
-            int take3 =
-                nums[i] + nums[i + 1] + nums[i + 2] - solve(i + 3, n, nums);
-            ans = max(ans, take3);
+        if(i+2 < n){
+            int take3 = nums[i] + nums[i+1] + nums[i+2] - solve(i+3,n,nums);
+            ans = max(ans,take3);
         }
 
         return dp[i] = ans;
     }
 
-    string stoneGameIII(vector<int>& stoneValue) {
-
+    string stoneGameIII(vector<int>& stoneValue) {  
         int n = stoneValue.size();
 
-        dp.assign(n, -1);
+        dp.assign(n,-1);
 
-        int ans = solve(0, n, stoneValue);
+        int ans = solve(0,n,stoneValue);
 
-        if (ans > 0)
+        if(ans>0){
             return "Alice";
-        else if (ans < 0)
+        } else if(ans<0){
             return "Bob";
+        }
 
         return "Tie";
+
     }
 };
